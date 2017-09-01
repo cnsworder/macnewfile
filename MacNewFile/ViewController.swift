1c5f98a5acd43915e7562738138ac800eb9b5377
bf.chen <bf.chen@unicall.net.cn>
2017-09-01 16:05:08 +0800

    Initial Commit

---
+++ MacNewFile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacNewFile
+//
+//  Created by 陈冰峰 on 01/09/2017.
+//  Copyright © 2017 MajorEssense. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
